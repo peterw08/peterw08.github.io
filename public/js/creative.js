57348edbd46de3910a51689650780b564f1a7b35
Peter Wright <peterw08@gmail.com>
2018-10-29 13:46:37 -0700

    commit3

--- public/js/creative.js
+++ public/js/creative.js
@@ -54,9 +54,9 @@
   $('input[name=url]').focus();
 
   $('input[name=url]').on('keyup', function(e){
-    var hash = ($(this).val().match(/https:\/\/docs\.google\.com\/document\/d\/(.+)\/pub/) || $(this).val().match(/http:\/\/gdoc\.pub\/(.+)/) || [])[1];
+    var hash = ($(this).val().match(/https:\/\/docs\.google\.com\/document\/d\/(.+)\/pub/) || $(this).val().match(/http:\/\/peterw08.github\.io\/(.+)/) || [])[1];
     if (hash) {
-      $(this).val('http://gdoc.pub/'+hash);
+      $(this).val('http://peterw08.github/'+hash);
       $(this).select();
       $('.docview').addClass('unrolled').find('a').attr('href', '/'+hash);
       $('.wrongurl').removeClass('unrolled');
